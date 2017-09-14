2229dd36fde894ea5237f97e527d02fd199b3879
Louis Huggins <lou@Louiss-MacBook-Pro.local>
2017-09-14 08:48:30 -0400

    adding comment

--- AlarmApp/app/src/main/java/com/create/lou/myapplication/JobSchedulerService.java
+++ AlarmApp/app/src/main/java/com/create/lou/myapplication/JobSchedulerService.java
@@ -4,6 +4,8 @@ import android.app.job.JobService;
 
 /**
  * Created by lou on 9/13/17.
+ *
+ * Adding comments to the code to see if there is a difference
  */
 
 public class JobSchedulerService extends JobService {
